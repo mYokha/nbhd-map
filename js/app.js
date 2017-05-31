452dbe99aa1e944d08a36a584243b0f41231f56c
Max Yokha <max.yokha@gmail.com>
2017-05-31 05:24:08 +0300

    Major improvements and bug fixes

--- js/app.js
+++ js/app.js
@@ -37,18 +37,19 @@ function Location(value, index) {
 
 	this.website = value.url || 'No link. You better Google it';
 	this.marker;
-	this.isClicked = false;
 }
+
 //Link to Udacity's APIs course repo: https://github.com/udacity/ud864
 function initMap() {
+
 	//Initialize google maps
 	var map;
+
 	//That's the place where Anfield is
 	var center = {
 		lat: 53.430849,
 		lng: -2.960862
 	};
-
 
 	// Create a map object and specify the DOM element for display.
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -91,7 +92,6 @@ function initMap() {
 		}
 	});
 
-
 	markerStadium.addListener('click', function () {
 		infowindow.open(map, markerStadium);
 	});
@@ -102,7 +102,6 @@ function initMap() {
 		// Get the position from the location array.
 		var position = appViewModel.myPubs()[i].position;
 		var title = appViewModel.myPubs()[i].title;
-
 
 		// Create a marker per location, and put into markers array.
 		appViewModel.myPubs()[i].marker = new google.maps.Marker({
@@ -117,6 +116,11 @@ function initMap() {
 		// Create an onclick event to open an infowindow at each marker.
 		appViewModel.myPubs()[i].marker.addListener('click', function () {
 			populateInfoWindow(this, largeInfowindow);
+
+			appViewModel.myPubs().forEach(function (item) {
+				item.marker.setAnimation(null);
+			});
+			this.setAnimation(google.maps.Animation.BOUNCE);
 		});
 
 		bounds.extend(appViewModel.myPubs()[i].position);
@@ -145,9 +149,8 @@ function getLocation(objArr, id) {
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-
 	// Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
+	if (infowindow.marker !== marker) {
 		infowindow.marker = marker;
 		var id = marker.id;
 		var location = getLocation(appViewModel.myPubs(), id);
@@ -161,7 +164,6 @@ function populateInfoWindow(marker, infowindow) {
 			'<span class="info-heading">Distance to Anfield:</span> ' + location.distance + 'm<br><br>' +
 			'<span class="info-heading">Verified Foursquare account:</span> ' + location.verified + '<br><br>' +
 			'<a href="' + location.foursquareProfileURL + '" target="_blank">Foursquare profile link</a><br><br>');
-		infowindow.open(map, marker);
 
 		// Make sure the marker property is cleared if the infowindow is closed.
 		infowindow.addListener('closeclick', function () {
@@ -169,6 +171,8 @@ function populateInfoWindow(marker, infowindow) {
 			marker.setAnimation(null);
 		});
 	}
+
+	infowindow.open(map, marker);
 }
 
 //Knockout's viewmodel
@@ -180,105 +184,71 @@ function AppViewModel() {
 	//Working around foursquare api
 	var client_id = 'ZRZOYO5I4TD5PDUZZOOQ4UNAVORKVMQJJDM5S4YUBNFHD05C';
 	var client_secret = 'LDAYVFPBDALCUPTK1DBDSZC4XF5AHGG1I5IWOTSNI4YW1WXT';
-	//var apiURL = 'https://api.foursquare.com/v2/venues/';
 	var placeId = '4baf7f4ff964a520bd043ce3';
-	//var foursquareURL = apiURL + placeId + '?client_id=' + client_id +  '&client_secret=' + client_secret +'&v=' + foursquareVersion;
-	//var foursquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=' + client_id + '&client_secret=' + client_secret + '&v=20130815&ll=53.430849,-2.960862&query=beer';
 	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=' + client_id + '&client_secret=' + client_secret + '&v=20130815&ll=53.430849,-2.960862&&radius=3000&categoryId=4bf58dd8d48988d155941735,52e81612bcbc57f1066b7a06,4bf58dd8d48988d11b941735';
 
 	function loadJSON(callback) {
-
-		var xobj = new XMLHttpRequest();
-		xobj.overrideMimeType("application/json");
-		xobj.open('GET', foursquareURL, false);
-		xobj.onreadystatechange = function () {
-			if (xobj.readyState == 4 && xobj.status == "200") {
-
-				// Required use of an anonymous callback as .open 
-				// will NOT return a value but simply returns undefined 
-				// in asynchronous mode
-				callback(xobj.responseText);
-			} else {
-				alert('Error has occured while processing Foursquare API request:\n' + xobj.status);
-			}
-		};
-		xobj.send(null);
+		fetch(foursquareURL)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (json) {
+				callback(json.response.venues);
+			})
+			.catch(function (error) {
+				alert('Oops.. the error has occured while loading data from Fousquare.\n\nError message:\n ' + error);
+			});
 	}
 
-	loadJSON(function (response) {
+	loadJSON(function (venues) {
+		venues.forEach(function (venue, i) {
+			self.myPubs.push(new Location(venue, i));
+		});
 
-		// Parse JSON string into array of objects
-		var results = JSON.parse(response).response.venues;
+		self.inputField = ko.observable('');
 
-		for (var i = 0; i < results.length; i++) {
-			self.myPubs.push(new Location(results[i], i));
-		}
+		// This observable is a need. When you put the cursor into the input field
+		// it makes list of titles visible and hides them when cursor is off.
+		self.isSelected = ko.observable(false);
 
-	});
+		// Now let's try to implement filtering
+		self.filteredItems = ko.computed(function () {
+			if (self.inputField().length > 0) {
+				return ko.utils.arrayFilter(self.myPubs(), function (item) {
+					var chosenItem = item.title.toLowerCase().indexOf(self.inputField().toLowerCase()) > -1;
+					if (!chosenItem) {
+						item.marker.setVisible(false);
+					} else {
+						item.marker.setVisible(true);
+					}
 
-
-	self.inputField = ko.observable('');
-
-	// This observable is a need. When you put the cursor into the input field
-	// it makes list of titles visible and hides them when cursor is off.
-	self.isSelected = ko.observable(false);
-
-	// Now let's try to implement filtering
-	self.filteredItems = ko.computed(function () {
-		if (self.inputField().length > 0) {
-			return ko.utils.arrayFilter(self.myPubs(), function (item) {
-				var chosenItem = item.title.toLowerCase().indexOf(self.inputField().toLowerCase()) > -1;
-				if (!chosenItem) {
-					item.marker.setVisible(false);
-				} else {
-					item.marker.setVisible(true);
+					return chosenItem;
+				});
+			} else {
+				for (var i = 0; i < self.myPubs().length; i++) {
+					if (self.myPubs()[i].marker) {
+						self.myPubs()[i].marker.setVisible(true);
+					}
 				}
 
-				return chosenItem;
-			});
-		} else {
-			for(var i = 0; i< self.myPubs().length; i++){
-				if (self.myPubs()[i].marker) {
-					self.myPubs()[i].marker.setVisible(true);
-				}
+				return self.myPubs();
 			}
-			
-			return self.myPubs();
-		}
-	});
+		});
 
-	self.liSelected = ko.observable();
+		self.liSelected = ko.observable();
 
-	self.isClickedReset = function (arr) {
-		for (var i = 0; i < arr.length; i++) {
-			arr[i].isClicked = false;
-			arr[i].marker.setAnimation(null);
-		}
-	};
-
-	// Activates marker... basically it opens marker's infowindow when
-	// click the list item
-	self.activateMarker = function (item) {
-		self.isClickedReset(self.myPubs());
-		item.isClicked = true;
-		if (item.isClicked) {
+		// Activates marker... basically it opens marker's infowindow when
+		// click the list item
+		self.activateMarker = function (item) {
 			google.maps.event.trigger(item.marker, 'click');
-			//item.marker.setAnimation(google.maps.Animation.BOUNCE);
-		} else {
-			item.marker.setAnimation(null);
-		}
-		self.liSelected(item.title);
 
-		if (item.isClicked) {
-			item.marker.setAnimation(google.maps.Animation.BOUNCE);
-		} else {
-			item.marker.setAnimation(null);
-		}
-	};
+			self.liSelected(item.title);
+		};
+		
+		initMap();
+	});
 }
 var appViewModel = new AppViewModel();
-
-
 
 // A bit of javascript to hide list when it's item is clicked if the screen is narrow
 if (window.innerWidth < 720) {
@@ -297,6 +267,7 @@ if (window.innerWidth < 720) {
 // Google maps loading error handler
 function mapsInitError() {
 	var mapElement = document.getElementById('map');
+	mapElement.setAttribute("data-bind", "text: errMsg")
 	mapElement.style.margin = 'auto';
 	mapElement.style.fontWeight = 700;
 	mapElement.style.fontSize = '48px';
--- readme.md
+++ readme.md
@@ -1,33 +1,16 @@
 # Pubs around Anfield
 
->Assume you're a huge LFC fan but you are not a local guy.
+## Short story
+Assume you're a huge LFC fan but you are not a local guy.
 Obviously, if you're in the town and you're about to watch the game you want to have a pint of beer.
 Where can you do that? Easy. This app will show you all the places in radius of like 3km from the stadium.
 
-
 ## How to use the app
+What is really great about the app is that it's fully client-sided so there's no need to set up a `localhost`.
+1. All you need to do is to use `git clone` in your Git bush or press "Clone or download" and then "Download ZIP". That's the way you get files on your machine.
+2. Run `index.html` in your favorite browser.
 
-### Mobile users first
->The app is fully responsive and mobile friendly.
-
-What mobile users need to know:
-- When you open an app you can see a map with markers of pubs.
-- To see the list of pubs click the input field
-- To filter the pubs start typing
-- When you hit the chosen pub it's name will be set as a default value of the input field
-- Attention! The list is hiding after you chose the item so you can the full map. If you put cursor into the input field again the list will appear. This is by design!
-
-What desktop users need to know:
-- Usage is very similar
-- The list of pubs appears on the left side of the window
-- Markers and pubs in the list filter as you are typing... cool ha? :)
-
-What every user needs to know:
-- This app uses Foursquare API to load the information about every single location
-- Foursquare information appears in the marker's infowindow
-- This app is pretty simple but the author sruggled sometimes writhing it... Just so you know.
-
-To start the app follow this [link](https://myokha.github.io/nbhd-map/).
+[Demo](https://myokha.github.io/nbhd-map/).
 
 #### Credits
 Thanks to [Udacity](udacity.com) for making me staying awake few nights in a row
